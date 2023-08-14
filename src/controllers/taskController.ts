b8e427b5c7fc49751eb3cf86105ed3a53aba1195
Joel Jaim <jowjaim20@gmail.com>
2023-08-14 20:06:00 +0800

    cheerio test

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as cheerio from "cheerio";
 import puppeteer from "puppeteer";
 
 const startTask = async (req: Request, res: Response) => {
@@ -10,7 +11,7 @@ const startTask = async (req: Request, res: Response) => {
       // "--single-process",
       "--no-zygote"
     ],
-    headless: "new",
+    headless: false,
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
@@ -18,16 +19,44 @@ const startTask = async (req: Request, res: Response) => {
   });
   const page = await browser.newPage();
   await page.goto(link);
-  // await page.screenshot({ path: "example.png" });
-  const tweetHandles = await page.$$("#product-loop");
-  console.log("tweetHandles", tweetHandles);
   const testArr: string[] = [];
 
-  for (const tweetHandle of tweetHandles) {
-    // @ts-ignore
-    const singleTweet = await page.evaluate((el) => el.innerText, tweetHandle);
-    testArr.push(singleTweet);
+  // await page.screenshot({ path: "example.png" });
+  let isDisabled = false;
+  while (!isDisabled) {
+    const $ = cheerio.load(await page.content());
+    const sib = $("#product-loop");
+    const child = sib.children();
+    child.each((i, el) => {
+      const text = $(el).find("div.product-info > div > a").text();
+      testArr.push(text.trim());
+    });
+
+    console.log("testArr", testArr);
+
+    // await page.waitForSelector("#pagination > a:nth-child(4)", {
+    //   visible: true
+    // });
+
+    const parentElement = $("#pagination");
+
+    // Check if the last span has the "current" class
+    const lastChild = parentElement.find(":last-child");
+    const isLastChildCurrent = lastChild.hasClass("current");
+    console.log("isLastSpanCurrent", isLastChildCurrent);
+
+    isDisabled = isLastChildCurrent;
+
+    if (!isLastChildCurrent) {
+      try {
+        await page.click("#pagination > a:nth-child(4)");
+      } catch (error) {
+        console.log(error);
+      }
+      // await page.waitForNavigation();
+    }
   }
+
   await browser.close();
 
   res.status(200).json({
